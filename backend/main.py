38441b6e4dc504f0ec6cad3dec93cfeaad1c1c0d
azhidee <alireza.azhide@gmail.com>
2023-07-14 23:04:57 +0330

    firstCommit

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from elasticsearch import Elasticsearch
+
+app = FastAPI()
+
+# es = Elasticsearch()
+
+@app.get('/')
+def read_root():
+    return {"testK":"testV"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id":item_id, "q":q}
+
+
